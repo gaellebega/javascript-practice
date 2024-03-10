bb2fd74fd371eb16931dde97ffe9463fbfa55eb7
gaellebega <gaellebega@gmail.com>
2024-03-10 11:04:34 +0200

    changes

--- main.js
+++ main.js
@@ -3,11 +3,9 @@ function Book(title, author) {
     this.title = title;
     this.author = author;
   }
-  
   function Library() {
     this.books = [];
   }
-  
   Library.prototype.addBook = function(title, author) {
     const book = new Book(title, author);
     this.books.push(book);
@@ -17,20 +15,12 @@ function Book(title, author) {
     this.books = this.books.filter(book => book.title !== title);
   };
   
-  
   Library.prototype.listBooks = function() {
     this.books.forEach(book => console.log(`${book.title} by ${book.author}`));
   };
-  
-  
   const library = new Library();
-  
-  
   library.addBook('To Kill a Mockingbird', 'Harper Lee');
   library.addBook('The Great Gatsby', 'F. Scott Fitzgerald');
-  
   library.listBooks();
-  
   library.removeBook('To Kill a Mockingbird');
-  
   library.listBooks();
